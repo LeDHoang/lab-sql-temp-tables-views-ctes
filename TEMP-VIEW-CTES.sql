91ca11910944e8a38259f6a5f7f17be1adba7e51
Hoang Le Duc <leduchoang011@gmail.com>
2024-05-22 23:54:36 +0700

    Update TEMP-VIEW-CTES.sql

--- TEMP-VIEW-CTES.sql
+++ TEMP-VIEW-CTES.sql
@@ -6,6 +6,8 @@ use sakila;
 -- Step 1: Create a View
 -- First, create a view that summarizes rental information for each customer.
 -- The view should include the customer's ID, name, email address, and total number of rentals (rental_count).
+DROP VIEW IF EXISTS rental_summary;
+
 CREATE VIEW rental_summary AS
 SELECT 
     c.customer_id,
@@ -16,21 +18,23 @@ FROM customer c
 JOIN rental r ON c.customer_id = r.customer_id
 GROUP BY c.customer_id, c.email;
 
-DROP VIEW IF EXISTS rental_summary;
 Select * from rental_summary;
 -- Step 2: Create a Temporary Table
 -- Next, create a Temporary Table that calculates the total amount paid by each customer (total_paid). 
 -- The Temporary Table should use the rental summary view created in Step 1 to join with the payment table and calculate the total amount paid by each customer.
+DROP TABLE IF EXISTS payment_summary;
+
 CREATE TEMPORARY TABLE payment_summary AS
 SELECT 
     c.customer_id,
     c.name,
     SUM(p.amount) AS total_paid
 FROM rental_summary c
-JOIN payment p ON c.customer_id = p.customer_id
+JOIN rental r ON c.customer_id = r.customer_id
+JOIN payment p ON r.rental_id = p.rental_id
 GROUP BY c.customer_id;
     
-DROP TABLE IF EXISTS payment_summary;
+
 select * from payment_summary;
 
 -- Step 3: Create a CTE and the Customer Summary Report
